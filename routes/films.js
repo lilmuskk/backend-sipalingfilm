509e009113c70588a01b31a239b638a606afea7d
nln <142864500+lilmuskk@users.noreply.github.com>
2025-06-11 16:49:49 +0700

    Routes

--- routes/films.js
+++ routes/films.js
@@ -6,12 +6,16 @@ const { protect } = require('../middleware/authMiddleware'); // Optional: protec
 // Routes for managing master film data
 // You might want to add an 'isAdmin' middleware here if only admins can CRUD films
 router.route('/')
-  .post(filmController.createFilm) // Create film
-  .get(filmController.getAllFilms); // Read all films
+  .post(filmController.createFilm) // Create film
+  .get(filmController.getAllFilms); // Read all films
 
 router.route('/:id')
-  .get(filmController.getFilmById) // Read single film by ID
-  .put(filmController.updateFilm) // Update film
-  .delete(filmController.deleteFilm); // Delete film
+  .get(filmController.getFilmById) // Read single film by ID
+  .put(filmController.updateFilm) // Update film
+  .delete(filmController.deleteFilm); // Delete film
+
+// --- TAMBAHKAN BARIS INI UNTUK RUTE PENCARIAN ---
+// Endpoint: GET /api/films/search?query=...
+router.get('/search', filmController.searchFilms); // <--- TAMBAHKAN BARIS INI
 
 module.exports = router;
